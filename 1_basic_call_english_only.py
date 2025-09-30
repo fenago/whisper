886d1dcd2a240bbe42910a5ec46af6f20b36e591
Dr. Ernesto Lee <socrates73@gmail.com>
2025-09-30 12:59:30 -0400

    Update 1_basic_call_english_only.py

--- 1_basic_call_english_only.py
+++ 1_basic_call_english_only.py
@@ -1,15 +1,21 @@
+# !pip install openai-whisper
+
 import whisper
-from pathlib import Path
+import urllib.request
 
-
+# Load the model
 MODEL = whisper.load_model("medium.en")
-AUDIO_DIR = Path(__file__).parent / "test_audio_files"
 
+# Download the audio file
+AUDIO_URL = "https://github.com/fenago/whisper/raw/refs/heads/main/test_audio_files/terrible_quality.mp3"
+AUDIO_FILE = "terrible_quality.mp3"
+
+urllib.request.urlretrieve(AUDIO_URL, AUDIO_FILE)
 
 def get_transcription(audio_file: str):
     result = MODEL.transcribe(audio_file)
     print(result)
     return result
 
-
-get_transcription(str(AUDIO_DIR / "terrible_quality.mp3"))
+# Run transcription
+get_transcription(AUDIO_FILE)
